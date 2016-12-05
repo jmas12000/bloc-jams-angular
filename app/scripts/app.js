062bbc1c587ea5d498af04a96918aa0e5094a0e8
Jeff Moore <jmas1_2000@hotmail.com>
2016-12-04 17:05:18 -0800

    assignment-3-routing: completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,25 @@
 (function() {
-     function config($stateProvider, $locationProvider) {
-         $locationProvider
-         .html5Mode({
-             enabled: true,
-             requireBase: false
-         });
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
          
-         $stateProvider
+        $stateProvider
             .state('landing', {
                 url: '/',
                 templateUrl: '/templates/landing.html'
             })
             .state('album', {
-             url: '/album',
-             templateUrl: '/templates/album.html'
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
+            .state('collections', {
+                url: '/collection',
+                templateUrl: '/templates/collection.html'
             });
-     }
+    }
     
      angular
          .module('blocJams', ['ui.router'])
